6329efeae358dbfcda14d6cb196c5e0548853527
Teragi <72382774+Teragi@users.noreply.github.com>
2022-02-02 16:12:17 +0100

    marketPlace

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const empty = document.getElementById("empty");
 const full = document.getElementById("full");
-
+// const del = document.querySelector("delete");
+const quant = document.getElementById("quantity2");
 // Popup cart
 
 function show(){
@@ -23,6 +24,7 @@ function productCount(product){
      total = newInputBtn + 1;
      empty.style.display ="none";
      full.style.display = "block";
+
     }
     if(product == false && newInputBtn > 0){
      total = newInputBtn - 1;
@@ -33,13 +35,20 @@ function productCount(product){
         full.style.display = "none";
     }
     if(total > 0){
-        empty.style.display = "none"
+        empty.style.display = "none";
 ;    }
     document.getElementById('quantity').innerText = document.getElementById('input').value = total;
     document.getElementById('quantity2').innerText = document.getElementById('input').value = total;
     
  }
 
+//  Delete
+// function suppr(){
+//     del.addEventListener('click', function(){
+//         empty.style.display ="block";
+//         full.style.display = "none";
+//     })
+// }
 //  Total + Add to cart
 
         // show.addEventListener('click', function(){
@@ -58,12 +67,16 @@ function productCount(product){
 
            const total = newInputBtn * 125;
            document.getElementById('total-price').innerText = total;
+           quant.style.display = "block";
+           empty.style.display ="none";
            console.log("Bouton add to cart cliqué");
         })
         
 // Slider
 
 const imgs = document.querySelectorAll('.slider-contents img');
+const suivant = document.querySelector('.arrow-right');
+const precedent = document.querySelector('.arrow-left');
 const slides = document.querySelectorAll('.desktopSlide');
 let index = 0;
 
@@ -82,7 +95,7 @@ slides.forEach(sliderContentsDesktop =>{
 suivant.addEventListener('click', slideSuivante);
 
 function slideSuivante(){
-    if(index < 2){
+    if(index < 3){
         imgs[index].classList.remove('active');
         index++;
         imgs[index].classList.add('active');
@@ -102,9 +115,8 @@ function slidePrecedente(){
         imgs[index].classList.add('active');
     }else if(index === 0){
         imgs[index].classList.remove('active');
-        index = 2;
+        index = 3;
         imgs[index].classList.add('active');
-
     }
 }
 
@@ -114,19 +126,21 @@ function slidePrecedente(){
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.querySelectorAll("myBtn");
+var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var image = document.getElementsByClassName("active");
 var modalImage = document.getElementById("imageModal");
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-  div.appendChild(image);
+  var img = document.createElement('img');
+  img.src = imgs[index].src;
+  modalImage.appendChild(img);
 }
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -139,22 +153,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-
-// imgs.addEventListener(click, function() {
-//   for(let i = 0; i < imgs.length; i++){
-
-//   }
-// })
-
-// const sliderContents = document.querySelectorAll('slider-contents');
-
-// imgs.forEach(sliderContents =>{
-//   sliderContents.addEventListener('click', function(){
-      
-//     document.div.innerHTML = imgs;
-//       this.classList.add('active-slide');
-//       imgs[index].classList.remove('active');
-//       index = this.getAttribute('data-clic') -1;
-//       imgs[index].classList.add('active');
-//   })
-// })
