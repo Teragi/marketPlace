36304cdc65ab33205db47b99f96efa31503c74f3
Teragi <72382774+Teragi@users.noreply.github.com>
2022-02-02 11:02:29 +0100

    marketPlace

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const nav = document.querySelector(".primary-navigation");
 const arrowPrevious = document.querySelector(".arrow-left");
 const arrowNext = document.querySelector(".arrow-right");
-const slider = document.querySelector(".slider")
-const addToCartButton = document.querySelectorAll(".add-to-cart");
-const remove = document.querySelector('.delete');
-const popup = document.querySelectorAll(".basket");
-const show = document.getElementById("show");
+const slider = document.querySelector(".slider");
+const popup = document.querySelector(".basket")
 
 // TEST
 
@@ -21,16 +17,29 @@ for(let i = 0; popup.length; i++){
     })
 }
 
-// function show(){
-//     let show = document.getElementById("show");
-//   if (show.style.display === "none") {
-//     show.style.display = "block";
-//   } else {
-//     show.style.display = "none";
-//   }
-// }
+function show(){
+    let show = document.getElementById("show");
+  if (show.style.display === "none") {
+    show.style.display = "block";
+  } else {
+    show.style.display = "none";
+  }
+}
 
 // Counter
+// main.addEventListener('click', (event) => {
+//     const minusButton = event.target.closest(".minus");
+//     const plusButton = event.target.closest(".plus");
+//     const display = document.querySelectorAll(".current-value");
+//     if(minusButton) {
+//         if(display.textContent === "0") return
+//             display.textContent--;
+//     }
+//     if(plusButton) {
+//         if(display.textContent === "50") return
+//             display.textContent++;
+//     }
+// })
 function productCount(product){
     const inputBtn = document.getElementById('input').value;
     const newInputBtn = parseInt(inputBtn);
@@ -56,95 +65,50 @@ function productCount(product){
            console.log("125$")
         })
 
+        // const calculatePrice = () => {
+        //     const amountOfPairs = document.querySelector(".current-value");
+        //     const value = parseInt(amountOfPairs.textContent);
+        //     const sneakersPrice = document.querySelector(".sneaker-price");
+        //     const xTimes = document.querySelector(".x-times");
+        //     const finalPrice = document.querySelector(".stronger");
+        //     const currentPrice = sneakersPrice.textContent;
+        //     const removeDolar = currentPrice.substring(1, 7);
+        //     const priceToNum = parseInt(removeDolar);
+        //     xTimes.textContent = "x" + value ;
+        //     finalPrice.textContent = "$" + value * priceToNum ; 
+        //  }
+        //  calculatePrice();
 // Add to cart button
-for(let i = 0; addToCartButton.length; i++){
-    addToCartButton[i].addEventListener('click', () => {
-        console.log("Bouton Add to cart cliqué");
-    })
-}
-
+// for(let i = 0; addToCartButton.length; i++){
+//     addToCartButton[i].addEventListener('click', () => {
+//         console.log("Bouton Add to cart cliqué");
+//     })
+// // }
+// addToCartButton.addEventListener('click', () => {
+//     const amountOfPairs = document.querySelector(".current-value");
+//     const basketItems = document.querySelector(".number-of-items");
+//     const value = parseInt(amountOfPairs.innerHTML)
+//     if(value > 0) {
+//         basketItems.textContent = value;
+//         calculatePrice();
+//     }    
+// })
 
 
 // Slider
 
 const imgs = document.querySelectorAll('.slider-contents img');
-const slides = document.querySelectorAll('.desktop-slide');
-
+const slides = document.querySelectorAll('.desktopSlide');
 let index = 0;
-suivant.addEventListener('click', slideSuivante);
 
-function slideSuivante(){
-    if(index < 3){
-        imgs[index].classList.remove('active');
-        //cercles[index].classList.remove('active-cercle');
-        index++;
-        imgs[index].classList.add('active');
-        //cercles[index].classList.add('active-cercle');
-    }else if(index === 2){ 
-        imgs[index].classList.remove('active');
-        //cercles[index].classList.remove('active-cercle');
-        index = 0;
-        imgs[index].classList.add('active');
-        //cercles[index].classList.add('active-cercle');  
-    }
-
-    for(let i = 0; i < slides.length;i++){
-        if(slides[i].getAttribute('data-clic') - 1 === index){
-            slides[i].classList.add('active-slide');
-        }else{
+slides.forEach(sliderContentsDesktop =>{
+    sliderContentsDesktop.addEventListener('click', function(){
+        for(let i = 0; i < slides.length; i++){
             slides[i].classList.remove('active-slide');
         }
-    }
-}
-
-precedent.addEventListener('click', slidePrecedente);
-
-function slidePrecedente(){
-    if(index > 0){
+        this.classList.add('active-slide');
         imgs[index].classList.remove('active');
-        //cercles[index].classList.remove('active-cercle');
-        index--;
+        index = this.getAttribute('data-clic') -1;
         imgs[index].classList.add('active');
-        //cercles[index].classList.add('active-cercle');
-    }else if(index === 0){
-        imgs[index].classList.remove('active');
-        //cercles[index].classList.remove('active-cercle');
-        index = 2;
-        imgs[index].classList.add('active');
-        //cercles[index].classList.add('active-cercle');
-    }
-
-    for(let i = 0; i < cercles.length;i++){
-        if(slides[i].getAttribute('data-clic') - 1 === index){
-            slides[i].classList.add('active-slide');
-        }else{
-            slides[i].classList.remove('active-slide');
-        }
-    }
-}
-
-
-
-
-// document.addEventListener('keydown', keyPressed);
-
-// function keyPressed(e){
-//     //e = l'objet de l'evenement
-//     if(e.keyCode === 37){
-//         slidePrecedente();
-//     }else if(e.keyCode === 39){
-//         slideSuivante();
-//     }
-// }
-
-// slides.forEach(slide =>{
-//     cercle.addEventListener('click', function(){
-//         for(let i = 0; i < slides.length; i++){
-//             slides[i].classList.remove('active-cercle');
-//         }
-//         this.classList.add('active-cercle');
-//         imgs[index].classList.remove('active');
-//         index = this.getAttribute('data-clic') -1;
-//         imgs[index].classList.add('active');
-//     })
-// })
+    })
+})
